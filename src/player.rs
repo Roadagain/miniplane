5c0d7a208fa435f48eeb56af859d1f864ecb6419
Roadagain <ringoh72@gmail.com>
2019-06-08 09:36:47 +0900

    ダメージを実装

--- src/player.rs
+++ src/player.rs
@@ -1,11 +1,18 @@
 #[derive(Debug)]
 pub struct Player {
-    life: u32,
+    life: i32,
 }
 
 impl Player {
-    fn new(life: u32) -> Self {
+    fn new(life: i32) -> Self {
         Player { life }
+    }
+
+    pub fn damage(&mut self, damage: i32) {
+        if damage < 0 {
+            return;
+        }
+        self.life -= damage;
     }
 }
 
@@ -23,5 +30,19 @@ mod test {
     fn default_life_is_20() {
         let player: Player = Default::default();
         assert_eq!(player.life, 20)
+    }
+
+    #[test]
+    fn life_can_be_negative() {
+        let mut player = Player::new(1);
+        player.damage(3);
+        assert_eq!(player.life, -2);
+    }
+
+    #[test]
+    fn negative_damage_is_0() {
+        let mut player: Player = Default::default();
+        player.damage(-4);
+        assert_eq!(player.life, 20);
     }
 }
